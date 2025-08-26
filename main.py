7c8e20e53e0f877737f508fd856d77ca7c9e6e72
livia19gomes <167575522+livia19gomes@users.noreply.github.com>
2025-08-26 08:34:04 -0300

    API estremamente correta

--- main.py
+++ main.py
@@ -20,6 +20,6 @@ except Exception as e:
 from view import *
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000)
 
 
--- view.py
+++ view.py
@@ -1,3 +1,4 @@
+
 from flask import Flask, jsonify, request, send_file, render_template
 from flask_cors import CORS
 from main import app, con
